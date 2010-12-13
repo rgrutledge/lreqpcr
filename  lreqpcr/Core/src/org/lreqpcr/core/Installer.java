e795ccd2546f6f834d19e4f36f2904884cb45212
RGRUTLEDGE <RGRUTLEDGE@89b03dd5-a532-1418-0ab6-f70475be75a0>
2010-12-13 19:33:58 +0000

    Last changes to the help set before formal release

---  lreqpcr/Core/src/org/lreqpcr/core/Installer.java
+++  lreqpcr/Core/src/org/lreqpcr/core/Installer.java
@@ -29,7 +29,7 @@ public class Installer extends ModuleInstall {
     public void restored() {
         WindowManager.getDefault().invokeWhenUIReady(new Runnable() {
             public void run(){
-                WindowManager.getDefault().getMainWindow().setTitle("LRE Analyzer Ver 0.6b Working Copy");
+                WindowManager.getDefault().getMainWindow().setTitle("LRE Analyzer Ver 0.61b Working Copy");
             }
         });
     }
